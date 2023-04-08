65c355e8d751e738130b6c9709de26c07adc2093
Zach Ruiz <powersbooth10@gmail.com>
2023-04-08 13:40:40 -0500

    assigned each instrument its corresponding sound to play on the press of a button. set up in switch statemnet

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let tom1 = new Audio("sounds/tom-1.mp3")
+
 let tom2 = new Audio("sounds/tom-2.mp3")
 let tom3 = new Audio("sounds/tom-3.mp3")
 let tom4 = new Audio("sounds/tom-4.mp3")
@@ -9,7 +9,42 @@ let crash = new Audio("sounds/crash.mp3")
 let drumElements = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drumElements.length; i++) {
-    drumElements[i].addEventListener("click", handleClick);
+    drumElements[i].addEventListener("click", function () {
+
+        let buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                let kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            default:
+        }
+    });
 }
 
 
